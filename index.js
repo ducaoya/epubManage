1abaaa6533701bee1258f9988e09c8427ace328d
ducaoya <ducaoya@gmail.com>
2022-03-23 15:26:33 +0800

    test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("koa2-cors");
 
 app.use(
     cors({
-        origin: "https://wss-ebook.doromolll.xyz", //只允许http://localhost:8080这个域名的请求
+        origin: "*", //只允许http://localhost:8080这个域名的请求
         maxAge: 5, //指定本次预检请求的有效期，单位为秒。
         credentials: true, //是否允许发送Cookie
         allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], //设置所允许的HTTP请求方法
